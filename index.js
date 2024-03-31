5c2c272141a8c097daabed4aa9752ae60d081134
Bre <breannaroncal@gmail.com>
2024-03-30 22:04:02 -0700

    Get Blockchain data from API and filter to only USD and get top 5

--- index.js
+++ index.js
@@ -7,7 +7,19 @@ const port = 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-app.get("/", (req, res) => {
+// Sort prices from highest to lowest
+function sortPrices(a, b) {
+    return b.price_24h - a.price_24h;
+}
+
+app.get("/", async (req, res) => {
+    const response = await axios.get("https://api.blockchain.com/v3/exchange/tickers");
+    // Only get cryptocurrency in USD
+    var responseUSD = response.data.filter(element => element.symbol.includes("-USD"));
+    
+    // Get top five prices
+    var topFiveResponseUSD = responseUSD.sort(sortPrices).slice(0, 5);
+    console.log(topFiveResponseUSD);
     res.render("index.ejs");
 })
 
